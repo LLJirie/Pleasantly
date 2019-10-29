b0a1a9c6a5b8795effb5dd657f78ebafe29942d6
Leisa Bryan <lljbryan123@gmail.com>
2019-10-29 11:45:55 -0700

    adding

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems);
+//   });
